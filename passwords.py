6051decd9c62c08702a8ae81951c4b4161ac0903
Chris Drost <drostie@komodo.(none)>
2011-01-10 06:30:22 +0100

    Skeleton for the coming passwords functionality.

---
+++ passwords.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# This file is a part of adso, which uses PySkein, which is licensed under the 
+# GPL. As far as I can understand, this means that this code must also be 
+# released under the GPL. Since I don't believe in the value of copyright, I 
+# would like apologize to later users for that fact. Nonetheless: 
+# 
+#     Copyright 2010 Chris Drost
+#     
+#     adso is free software: it can be redistributed and modified under the 
+#     terms of the GNU General Public License, version 3, as published by the 
+#     Free Software Foundation. adso is distributed WITHOUT ANY WARRANTIES; 
+#     this includes the implied warranties of MERCHANTABILITY and FITNESS FOR A 
+#     PARTICULAR PURPOSE. See the license for more details. You should have 
+#     received a copy of the license text along with adso, in a text document 
+#     named 'COPYING'. If you have not, visit http://www.gnu.org/licenses/ .
+
+#from adso.core import adso
+#from adso.utils import lmap, terminal_display, traversible
+
+
